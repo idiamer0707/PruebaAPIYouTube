c97f4bd3a6b38dcb2239221f685b4b6592171f3d
idiamer0707 <157150292+idiamer0707@users.noreply.github.com>
2025-03-27 11:05:49 +0100

    Update PruebaAPIYouTube.js

--- PruebaAPIYouTube.js
+++ PruebaAPIYouTube.js
@@ -81,7 +81,7 @@ document.getElementById('fetchSubscribers').addEventListener('click', async () =
 
 const clientId = '126569545869-g7jdjkh3vmmc4g0acbv0862hat3i5l4m.apps.googleusercontent.com';
 const clientSecret = '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';
-const redirectUri = 'https://127.0.0.1:30000';
+const redirectUri = 'https://idiamer0707.github.io/PruebaAPIYouTube/';
 const scope = 'https://www.googleapis.com/auth/youtube.readonly';
 
 let accessToken = '';
